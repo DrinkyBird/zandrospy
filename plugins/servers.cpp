7eb5ba1886f3728212f26da7aa8d333cd6d99c99
Sean Baggaley <sean@csnxs.uk>
2023-02-01 20:46:25 +0000

    Add servers_response plugin

---
+++ plugins/servers.cpp
@@ -0,0 +1,70 @@
+#include <map>
+#include "muninregistration.h"
+
+REGISTER_PLUGIN(servers) {
+    if (ctx.isConfig()) {
+        ctx.write("graph_title Total servers");
+        ctx.write("graph_category servers");
+        ctx.write("servers.label Servers");
+        ctx.write("servers.min 0");
+    }
+
+    if (ctx.isFetch()) {
+        int num = 0;
+
+        ctx.lockServerData();
+        for (const auto &pair : ctx.getServerData()) {
+            if (pair.second.success()) {
+                num++;
+            }
+        }
+        ctx.unlockServerData();
+
+        ctx.writef("servers.value %d", num);
+    }
+}
+
+static std::map<uint32_t, std::string> RESPONSE_MAP = {
+    { 5660023, "Success" },
+    { 5660024, "Too fast" },
+    { 5660025, "Banned" },
+    { -1, "Timed out" }
+};
+
+REGISTER_PLUGIN(servers_response) {
+    std::unordered_map<uint32_t, int> map;
+    for (const auto &pair : RESPONSE_MAP) {
+        map[pair.first] = 0;
+    }
+
+    ctx.lockServerData();
+    for (const auto &pair : ctx.getServerData()) {
+        const auto &server = pair.second;
+        if (map.count(server.response) == 0) {
+            map[server.response] = 1;
+        } else {
+            map[server.response]++;
+        }
+    }
+    ctx.unlockServerData();
+
+    if (ctx.isConfig()) {
+        ctx.write("graph_title Server responses");
+        ctx.write("graph_category servers");
+
+        for (const auto &pair : map) {
+            if (RESPONSE_MAP.count(pair.first)) {
+                ctx.writef("%d.label %s", pair.first, RESPONSE_MAP[pair.first].c_str());
+            } else {
+                ctx.writef("%d.label %d", pair.first, pair.first);
+            }
+            ctx.writef("%d.min 0", pair.first);
+        }
+    }
+
+    if (ctx.isFetch()) {
+        for (const auto &pair : map) {
+            ctx.writef("%d.value %d", pair.first, pair.second);
+        }
+    }
+}
--- plugins/testplugin.cpp
+++ plugins/testplugin.cpp
@@ -1,28 +1,5 @@
 #include "muninregistration.h"
 
-REGISTER_PLUGIN(servers) {
-    if (ctx.isConfig()) {
-        ctx.write("graph_title Total servers");
-        ctx.write("graph_category servers");
-        ctx.write("servers.label Servers");
-        ctx.write("servers.min 0");
-    }
-
-    if (ctx.isFetch()) {
-        int num = 0;
-
-        ctx.lockServerData();
-        for (const auto &pair : ctx.getServerData()) {
-            if (pair.second.success()) {
-                num++;
-            }
-        }
-        ctx.unlockServerData();
-
-        ctx.writef("servers.value %d", num);
-    }
-}
-
 REGISTER_PLUGIN(players) {
     if (ctx.isConfig()) {
         ctx.write("graph_title Total players");
